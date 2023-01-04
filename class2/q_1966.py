a192db6c31f6ba283d7827a59864ca04c8a3710b
User <songhow53@naver.com>
2023-01-04 19:19:23 +0900

    feat:1966

--- class2/q_1966.py
+++ class2/q_1966.py
@@ -1,9 +1,22 @@
 import sys
 
 T = int(sys.stdin.readline())
-n_arr = [], turn_arr = [], pri_arr = []
 for i in range(T):
+    n, turn = map(int, sys.stdin.readline().split())
+    pri = list(map(int, sys.stdin.readline().split()))
+    queue = [0] * n
+    queue[turn] = 1
+    result = 0
+    while(len(pri) != 0):
+        G = max(pri)
+        if(pri[0] == G):
+            pri.pop(0)
+            n = queue.pop(0)
+            result += 1
+            if(n == 1):
+                print(result)
+                break
 
-
-for i in range(T):
-    q = [0] * n_arr
+        else:
+            pri.append(pri.pop(0))
+            queue.append(queue.pop(0))
