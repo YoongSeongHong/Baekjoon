617baf6d2dbabe196fcc418bebfbe142f80ee74b
User <songhow53@naver.com>
2023-01-05 20:44:09 +0900

    feat:1920

---
+++ class2/q_1920.py
@@ -0,0 +1,28 @@
+import sys
+def search(num):
+    left = 0
+    right = arr_len - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == num:
+            return True
+
+        if num < arr[mid]:
+            right = mid - 1
+        elif num > arr[mid]:
+            left = mid + 1
+
+
+arr_len = int(sys.stdin.readline())
+arr = list(map(int, sys.stdin.readline().split()))
+arr.sort()
+
+count = int(sys.stdin.readline())
+nums = list(map(int, sys.stdin.readline().split()))
+
+for i in range(count):
+
+    if search(nums[i]):
+        print(1)
+    else:
+        print(0)
