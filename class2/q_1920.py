ef9a750aabf6cdc1fb597e66bcb7cc28e4e6a87e
User <songhow53@naver.com>
2023-01-05 20:10:16 +0900

    feat:몰루

---
+++ class2/q_1920.py
@@ -0,0 +1,18 @@
+import sys
+
+arr_len = int(sys.stdin.readline())
+arr = list(map(int, sys.stdin.readline().split()))
+
+count = int(sys.stdin.readline())
+nums = list(map(int, sys.stdin.readline().split()))
+
+# for i in range(count):
+#     flag = 0
+#     for j in range(arr_len):
+#         if nums[i] == arr[j]:
+#             print(1)
+#             flag = 1
+#     if flag == 0:
+#         print(0)
+
+
