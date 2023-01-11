739ba1aa298bcc8789a5ccf91806681844169059
User <songhow53@naver.com>
2023-01-11 17:24:08 +0900

    feat:18111 마크 쉬벨롬 깼다

--- class2/q_18111.py
+++ class2/q_18111.py
@@ -9,31 +9,29 @@ for i in range(N):
 
 mx = max(lst1)
 mn = min(lst1)
-times = []
+# 리스트 정렬 안하면 시간 초과 뜸
+lst1.sort(reverse=True)
 
+result_sec = 9999999999
+result_h = 0
+
+# 최솟값 부터 최댓값 까지 다 돌려 볼거
 for i in range(mn, mx+1):
-    lst2 = lst1.copy()
     block = B
     time = 0
-    flag = 0
-    for j in range(len(lst2)):
-        if lst2[j] > i:
-            get = lst2[j] - i
-            lst2[j] = i
+    for j in lst1:
+        # 원하는 층 수 보다 높게 쌓여 있으면 캐기
+        if j > i:
+            get = j - i
             time += 2*get
             block += get
-    for k in range(len(lst2)):
-        if lst2[k] < i:
-            use = i - lst2[k]
-            lst2[k] = i
+        # 원하는 층 수 보다 낮게 쌓여 있으면 쌓기
+        elif j < i:
+            use = i - j
             block -= use
-            time += 1*use
-        if block < 0:
-            flag = 1
-            break
-    if flag == 0:
-        l = [time, i]
-        times.append(l)
-times = sorted(times, key=lambda times: times[1], reverse=True)
-times = sorted(times, key=lambda times: times[0])
-print(times[0][0], times[0][1])
+            time += use
+    # 블록 개수 안 모자라고 result_sec 보다 적게 걸리면 갱신. 같은 초 나와도 층 수 높은 값으로 갱신 하는 구조
+    if block >= 0 and result_sec >= time:
+        result_h = i
+        result_sec = time
+print(result_sec, result_h)
