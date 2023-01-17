e384388819ceb11c66e86f3ecc68ae72eff86cee
User <songhow53@naver.com>
2023-01-17 22:34:09 +0900

    feat:9375 주석 추가

--- class3/q_9375.py
+++ class3/q_9375.py
@@ -9,9 +9,11 @@ for i in range(T):
     for j in range(n):
         key, value = sys.stdin.readline().split()
         if value in dic:
+            # 키에 옷 종류, 값에 종류 수 저장
             dic[value] += 1
         else:
             dic[value] = 1
     for v in dic.values():
+        # v+1Cv = v+1. 예를 들어 종류가 3 3 2면 4 * 4 * 3 - 1
         com *= (v+1)
     print(com - 1)
